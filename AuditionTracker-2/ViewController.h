b552904ae94b032fa08b463dd0494fbbbd2cebd3
PT <PT@PTs-MacBook-Pro.local>
2015-05-22 09:14:44 -0600

    Initial Commit

---
+++ AuditionTracker-2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AuditionTracker-2
+//
+//  Created by PT on 5/22/15.
+//  Copyright (c) 2015 msse650. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AuditionTracker-2Tests/AuditionTracker_2Tests.m
@@ -0,0 +1,40 @@
+//
+//  AuditionTracker_2Tests.m
+//  AuditionTracker-2Tests
+//
+//  Created by PT on 5/22/15.
+//  Copyright (c) 2015 msse650. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface AuditionTracker_2Tests : XCTestCase
+
+@end
+
+@implementation AuditionTracker_2Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
