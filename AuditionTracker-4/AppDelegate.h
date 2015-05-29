79815b04a7c3dd1da490e9cc5f8e74287aa1a712
PT <PT@PTs-MacBook-Pro.local>
2015-05-29 07:53:00 -0600

    Initial Commit

---
+++ AuditionTracker-4/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AuditionTracker-4
+//
+//  Created by PT on 5/29/15.
+//  Copyright (c) 2015 msse650. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AuditionTracker-4/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AuditionTracker-4
+//
+//  Created by PT on 5/29/15.
+//  Copyright (c) 2015 msse650. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
