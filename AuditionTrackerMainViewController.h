3e5b773b1dea445a73fccf76deb73ba3f378985f
PT <PT@PTs-MacBook-Pro.local>
2015-06-20 19:51:02 -0600

    Wk7-CoreData

---
+++ AuditionTrackerMainViewController.h
@@ -0,0 +1,23 @@
+//
+//  AuditionTrackerMainViewController.h
+//  AuditionTracker-4
+//
+//  Created by PT on 6/15/15.
+//  Copyright (c) 2015 msse650. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AuditionTrackerMainViewController : UIViewController
+
+
+
+
+@property (weak, nonatomic) IBOutlet UIButton *myAuditionsButtonMain;
+
+
+@property (weak, nonatomic) IBOutlet UIButton *addAuditionButtonMain;
+
+
+
+@end
---
+++ AuditionTrackerMainViewController.m
@@ -0,0 +1,63 @@
+//
+//  AuditionTrackerMainViewController.m
+//  AuditionTracker-4
+//
+//  Created by PT on 6/15/15.
+//  Copyright (c) 2015 msse650. All rights reserved.
+//
+
+#import "AuditionTrackerMainViewController.h"
+#import "InfoViewController.h"
+
+
+@interface AuditionTrackerMainViewController ()
+
+
+
+
+
+@end
+
+@implementation AuditionTrackerMainViewController
+
+
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+   /*
+
+    [[_myAuditionsButtonMain layer] setBorderWidth:1.0f];
+    _myAuditionsButtonMain.layer.borderColor = [[UIColor blueColor] CGColor];
+    _myAuditionsButtonMain.layer.cornerRadius = 4;
+
+    
+
+
+    
+    [[_addAuditionButtonMain layer] setBorderWidth:1.0f];
+    _addAuditionButtonMain.layer.borderColor = [[UIColor blueColor] CGColor];
+    _addAuditionButtonMain.layer.cornerRadius = 4;
+    */
+
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (IBAction)infoPopUp:(id)sender {
+}
+@end
